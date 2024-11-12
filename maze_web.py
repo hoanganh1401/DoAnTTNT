61a2e3d84a5602760b8e40ee5b35f8e4466f0652
hoanganh1401 <149659552+hoanganh1401@users.noreply.github.com>
2024-11-13 00:16:43 +0700

    Update maze_web.py

--- maze_web.py
+++ maze_web.py
@@ -88,7 +88,7 @@ class MazeSolver(SearchProblem):
 
 st.title('Maze Path Finder')
 
-bg_image = Image.open("maze.bmp")
+bg_image = Image.open("maze.png")
 canvas_result = st_canvas(
     fill_color="rgba(255, 165, 0, 0.2)",
     stroke_width=5,
